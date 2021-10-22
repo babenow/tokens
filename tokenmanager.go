ee3982690ddeb4fecd2f5d1472b6d6e744aefe87
Maksin Babenko <babenoff.code@outlook.com>
2021-10-22 23:47:56 +0300

    JWT Token Manager

--- tokenmanager.go
+++ tokenmanager.go
@@ -1,4 +1,13 @@
 package tokens
 
+import "time"
+
+type TMConfig struct {
+	Secret              string
+	AccessTokenExpired  time.Duration
+	RefreshTokenExpired time.Duration
+}
+
 type TokenManager interface {
+	GenerateAccessToken(username string)
 }
